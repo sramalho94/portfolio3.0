edce6004a5079490a8a7d03598b90292a1bcc5af
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-13 19:19:33 -0400

    added section for about

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Image from 'next/image'
+import About from '@/components/About'
 
 export default function Home() {
   return (
@@ -8,12 +9,15 @@ export default function Home() {
       {/* header */}
       <Header />
       {/* Hero */}
-      <section id="hero" className="snap-center">
+      <section id="hero" className="snap-start">
         <div>
           <Hero />
         </div>
       </section>
       {/* About */}
+      <section id="about" className="snap-center">
+        <About />
+      </section>
 
       {/* Experience */}
 
