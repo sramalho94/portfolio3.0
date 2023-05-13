aa725ea8ccc98ad76a40da9677c3cc7727512822
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-13 11:55:10 -0400

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,17 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        violet: '#8155BA',
+        ebony: '#281C2D',
+        'purple-haze': '#BEAFC2',
+        'port-purple': '#695E93'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
