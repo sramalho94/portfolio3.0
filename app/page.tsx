02c2461a5ffd795703274a8df8da9189805e5711
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-13 12:25:10 -0400

    changes to utility classNames

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
+import Header from '@/components/Header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-port-purple">Welcome to my portfolio</h1>
-
+    <main className="flex min-h-screen flex-col p-12">
       {/* header */}
-
+      <Header />
       {/* Hero */}
 
       {/* About */}
