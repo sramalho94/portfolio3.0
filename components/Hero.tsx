87c0c1bb08852e2148becf926a4dd3b80e0da97f
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-13 18:44:28 -0400

    added features and changing positioning

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,15 +3,16 @@
 import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircle from './BackgroundCircle'
+import Image from 'next/image'
 
 type Props = {}
 
 const Hero = (props: Props) => {
   const [text, count] = useTypewriter({
     words: [
-      `import {Stephan D Ramalho} from "Queens"`,
+      `import {Stephan D Ramalho}`,
       `touch guyWhoLovesPizza.tsx`,
-      '<ButLovesToCode More />'
+      '<ButLovesToCodeMore/>'
     ],
     loop: true,
     delaySpeed: 2000
@@ -20,10 +21,31 @@ const Hero = (props: Props) => {
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <div className="relative">
         <BackgroundCircle />
-        <h1 className="absolute inset-0 flex justify-center items-center">
-          <span>{text}</span>
-          <Cursor cursorColor="#BEAFC2" />
-        </h1>
+        <div className="absolute inset-0 flex flex-col justify-center items-center">
+          <Image
+            src="/Profile.jpg"
+            alt="profile-pic"
+            className="rounded-full h-32 w-32"
+            width={128}
+            height={128}
+          />
+          <div>
+            <h2 className="text-sm uppercase text-violet pt-3 tracking-[13px] md:tracking-[15px]">
+              Software Engineer
+            </h2>
+            <h1 className="mt-4 text-3xl lg:text-4xl font-semibold px-3 md:px-10 w-[calc(100%-1rem)] md:w-2xl whitespace-pre-wrap md:whitespace-nowrap">
+              <span className="mr-3 justify-center">{text}</span>
+              <Cursor cursorColor="#BEAFC2" />
+            </h1>
+          </div>
+
+          <div>
+            <button>AAbout</button>
+            <button>Experience</button>
+            <button>Skills</button>
+            <button>Projects</button>
+          </div>
+        </div>
       </div>
     </div>
   )
