8c6f3fda8f3f15892892319bbad958a076cf00e8
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 19:04:27 -0400

    more sanity configuration

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,3 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import pageInfo from './pageInfo'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [pageInfo]
--- sanity/schemas/pageInfo.ts
+++ sanity/schemas/pageInfo.ts
@@ -36,5 +36,26 @@ export default defineType({
         hotspot: true,
       },
     },
+    {
+      name: 'phoneNumber',
+      title: 'PhoneNumber',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+    },
+    {
+      name: 'socials',
+      title: 'Socials',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'social'}}],
+    },
   ],
 })
