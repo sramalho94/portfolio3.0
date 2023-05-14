99a383a27139bee45fb3aefe90dce8627b5cd108
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 09:39:22 -0400

    mobile view positioning changes

--- components/About.tsx
+++ components/About.tsx
@@ -13,7 +13,23 @@ const About = (props: Props) => {
         About
       </h3>
 
-      <motion.img src="/Profile.jpg" />
+      <motion.img
+        src="/Profile.jpg"
+        transition={{ duration: 1.2 }}
+        initial={{ x: -200, opacity: 0 }}
+        viewport={{ once: true }}
+        whileInView={{ opacity: 1, x: 0 }}
+        className="mt-32 flex-shrink-0 w-56 h-56 rounded-full object-cover md:mb-0 md:mt-0 md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]"
+      />
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{' '}
+          <span className="underline decoration-[#695E93]/50">little</span>{' '}
+          background
+        </h4>
+        <p className="text-base md:text-lg">{`I'm Stephan D. Ramalho, a Queens-based Fullstack Software Engineer. Starting with a history degree from Fordham University, and transitioning through a customer-focused sales role, I found my passion in software engineering at General Assembly. Now a freelance developer, I prioritize user-friendly applications and teamwork. Away from coding, I delve into PC and PS5 gaming, enhancing my creative approach to tech solutions. I'm eager to bring my unique experience and passion to future collaborations.`}</p>
+      </div>
     </div>
   )
 }
