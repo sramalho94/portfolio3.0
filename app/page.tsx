dcc7f38184f45215274e34d01bb29ab1351c2c85
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 14:15:25 -0400

    incorporated Projects component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import About from '@/components/About'
 import Experience from '@/components/Experience'
 import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
 
 export default function Home() {
   return (
@@ -34,6 +35,9 @@ export default function Home() {
       </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact me */}
     </main>
