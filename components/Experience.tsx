212b5f7dae9e78c650692338132f9a9a002ed60a
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 12:29:10 -0400

    styling changes to experience card

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+'use client'
 
+import React from 'react'
+import { motion } from 'framer-motion'
+import ExperienceCard from './ExperienceCard'
 type Props = {}
 
 const Experience = (props: Props) => {
   return (
-    <div className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen flex relative overflow-x-scroll flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-2xl">
         Experience
       </h3>
-    </div>
+
+      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
+        {/* experience cards */}
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
+      </div>
+    </motion.div>
   )
 }
 
