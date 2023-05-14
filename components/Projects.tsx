c3b7da49c4ad9a2d0073bfc6794c94d9fcb64ef8
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 14:18:27 -0400

    styling for container div

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 type Props = {}
 
 const Projects = (props: Props) => {
-  return <div>Projects</div>
+  return <div className="h-screen">Projects</div>
 }
 
 export default Projects
