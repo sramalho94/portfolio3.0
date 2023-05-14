8ed09c75a1c9e5af4f5bc11e42463596a32e30e0
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 16:22:17 -0400

    added functionality to form through onSubmit

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,9 +1,29 @@
+'use client'
+
 import React from 'react'
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 type Props = {}
 
+type Inputs = {
+  name: string
+  email: string
+  subject: string
+  message: string
+}
+
 const Contact = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:stephanramalho@gmail.com?subject=${formData.subject}&body=Hello, my name is ${formData.name}. ${formData.message} (${formData.email})`
+  }
+
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center ">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-2xl">
@@ -29,15 +49,37 @@ const Contact = (props: Props) => {
           </div>
         </div>
 
-        <form className="flex flex-col space-y-2 w-fit mx-auto">
+        <form
+          className="flex flex-col space-y-2 w-fit mx-auto"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="flex space-x-2">
-            <input placeholder="Name" className="contactInput" type="text" />
-            <input placeholder="Email" className="contactInput" type="email" />
+            <input
+              {...register('name')}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register('email')}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
           </div>
 
-          <input placeholder="Subject" className="contactInput" type="text" />
+          <input
+            {...register('subject')}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
 
-          <textarea placeholder="Message" className="contactInput" />
+          <textarea
+            {...register('message')}
+            placeholder="Message"
+            className="contactInput"
+          />
           <button className="bg-purple-haze py-5 px-10 rounded-md text-ebony font-bold text-lg">
             Submit
           </button>
