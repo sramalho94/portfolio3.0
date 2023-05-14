1b0829535e184dd21f343f7ff55c77f372565441
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 10:43:34 -0400

    created section for experience

--- app/page.tsx
+++ app/page.tsx
@@ -2,24 +2,30 @@ import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Image from 'next/image'
 import About from '@/components/About'
+import Experience from '@/components/Experience'
 
 export default function Home() {
   return (
     <main className="h-screen flex-col bg-ebony text-violet snap-y snap-mandatory overflow-scroll z-0">
       {/* header */}
       <Header />
+
       {/* Hero */}
       <section id="hero" className="snap-start">
         <div>
           <Hero />
         </div>
       </section>
+
       {/* About */}
       <section id="about" className="snap-center">
         <About />
       </section>
 
       {/* Experience */}
+      <section id="experience" className="snap-center">
+        <Experience />
+      </section>
 
       {/* Skills */}
 
