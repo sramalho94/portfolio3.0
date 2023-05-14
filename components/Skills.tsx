e4f7a0d83cee344b74d1f09916e366b554bd067b
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 14:12:13 -0400

    styling changes to Skills

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -8,12 +8,26 @@ type Props = {}
 
 const Skills = (props: Props) => {
   return (
-    <motion.div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-2xl">
         Skills
       </h3>
       <h3 className="absolute top-36 uppercase tracking-[3px] text-port-purple text-sm">{`Hover over a skill for the number of projects I've created that incorporate it`}</h3>
-      <div>
+      <div className="grid grid-cols-4 gap-5 ">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
         <Skill />
       </div>
     </motion.div>
