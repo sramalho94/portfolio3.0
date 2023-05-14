5f7c5539f3ca7779a8d89d5a61e71689b4cc8c71
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-14 13:51:23 -0400

    imported Skill component

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,9 +1,23 @@
+'use client'
+
 import React from 'react'
+import { motion } from 'framer-motion'
+import Skill from './Skill'
 
 type Props = {}
 
 const Skills = (props: Props) => {
-  return <div>Skills</div>
+  return (
+    <motion.div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-2xl">
+        Skills
+      </h3>
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-port-purple text-sm">{`Hover over a skill for the number of projects I've created that incorporate it`}</h3>
+      <div>
+        <Skill />
+      </div>
+    </motion.div>
+  )
 }
 
 export default Skills
