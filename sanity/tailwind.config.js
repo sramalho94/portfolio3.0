36582b4251378f4a61a19ea0b07ee15603f30104
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-15 10:13:42 -0400

    added tailwindconfig to sanity

---
+++ sanity/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        violet: '#8155BA',
+        ebony: '#281C2D',
+        'purple-haze': '#BEAFC2',
+        'port-purple': '#695E93',
+      },
+      borderColor: (theme) => ({
+        ...theme('colors'),
+      }),
+    },
+  },
+  plugins: [require('tailwind-scrollbar')],
+}
